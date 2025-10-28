4456e86815184e012ef4a12bcbfa17af2ab5a9e0
Jorge G <jgeronim@akamai.com>
2025-10-28 14:38:34 -0700

    added local variable in main.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "edgerc_path" {
+  type        = string
+  description = "Path to the edgerc file"
+}
+
+variable "config_section" {
+  type        = string
+  description = "Config section in the edgerc file"
+}
