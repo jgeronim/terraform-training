9829b3179331b647a3f53cd5734e7c09cf6634a3
Jorge G <jgeronim@akamai.com>
2025-10-29 07:27:11 -0700

    moved local variable from locals.tf to main.tf

--- locals.tf
+++
@@ -1,7 +0,0 @@
-locals{
-
-    property_hostnames = [
-        for h in data.akamai_property_hostnames.my-property-hostnames.hostnames : h.cname_from
-    ]
-
-}
--- session4/main.tf
+++ session4/main.tf
@@ -1,5 +1,5 @@
 locals {
-    property_hostnames = [
+  property_hostnames = [
         for h in data.akamai_property_hostnames.my-property-hostnames.hostnames : h.cname_from
     ]
   notes = "${data.akamai_group.jg_tf_group.id}.TF-3001}"
