1d4b87aaefb78b2eae27a87660ba1340bfb677e5
Jorge G <jgeronim@akamai.com>
2025-10-29 15:23:19 -0700

    added akamai_property_activation resources with activation dependency, for Dependencies exercise

--- session4/property.tf
+++ session4/property.tf
@@ -14,11 +14,29 @@ resource "akamai_property" "jg_property" {
   }
 }
 
-
 resource "akamai_edge_hostname" "jg_edge_hostname" {
  contract_id = data.akamai_group.jg_tf_group.contract_id
  group_id    = data.akamai_group.jg_tf_group.id
  product_id = "prd_HTTP_Content_Del"
  edge_hostname = var.edge_hn == "A" ? "nobodycaresworkharder-2.me.edgesuite.net" : "nobodycaresworkharder-3.me.edgesuite.net"
  ip_behavior   = "IPV4"
+}
+
+resource "akamai_property_activation" "jgeronim_activation_1" {
+     property_id = data.akamai_property.jg_property.id
+     contact = ["jorge@nobodycaresworkharder.me"]
+     network = "STAGING"
+     note = "Sample activation 1"
+     version = 1
+}
+
+resource "akamai_property_activation" "jgeronim_activation_2" {
+     property_id = data.akamai_property.jg_property.id
+     contact = ["jorge@nobodycaresworkharder.me"]
+     network = "PRODUCTION"
+     note = "Sample activation 2"
+     version = 1
+     depends_on = [
+      akamai_property_activation.jgeronim_activation_1
+      ]
 }
