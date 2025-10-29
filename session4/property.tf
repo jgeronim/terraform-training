0af89f62cdc1ff7804deb17e61493e3853ba56a6
Jorge G <jgeronim@akamai.com>
2025-10-29 12:21:38 -0700

    added dynamic block using for iteration to auto-generate 5 different hostname blocks, for Dynamic Blocks exercise

--- session4/property.tf
+++ session4/property.tf
@@ -1,16 +1,19 @@
-/*
+
 resource "akamai_property" "jg_property" {
   name        = data.akamai_property.jg_property.name
   product_id  = data.akamai_property.jg_property.product_id
   contract_id = data.akamai_group.jg_tf_group.contract_id
   group_id    = data.akamai_group.jg_tf_group.id
-  hostnames {
-    cname_from = data.akamai_property_hostnames.my-property-hostnames.hostnames[0].cname_from
-    cname_to   = akamai_edge_hostname.jg_edge_hostname.edge_hostname
-    cert_provisioning_type = "DEFAULT"
+  dynamic "hostnames" {
+    for_each = local.app_hostnames
+    content {
+      cname_from = hostnames.value
+      cname_to   = akamai_edge_hostname.jg_edge_hostname.edge_hostname
+      cert_provisioning_type = "DEFAULT"
+    } 
   }
 }
-*/
+
 
 resource "akamai_edge_hostname" "jg_edge_hostname" {
  contract_id = data.akamai_group.jg_tf_group.contract_id
